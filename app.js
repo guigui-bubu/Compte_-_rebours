f6221ad42a30d60a0c646e0619a41680f29fd79b
guigui-bubu <guillaumebuaud@yahoo.fr>
2021-08-26 15:29:30 +0200

    new-world

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const text = document.querySelector('h2');
 function getChrono(){
 
     const now = new Date().getTime();    // nombre de ms qu'il y a eu depuis le 01/01/1970
-    const countDownDate = new Date('December 31, 2021').getTime();  // nombre ms pour aller au 31/12/2021
+    const countDownDate = new Date('September 09, 2021').getTime();  // nombre ms pour aller au 31/12/2021
     
     const distanceBase = countDownDate - now;  //   nombre de ms qui reste pour aller au 31/12/2021
 
@@ -11,8 +11,8 @@ function getChrono(){
     const hours = Math.floor((distanceBase % (1000 * 60 * 60 * 24)) / (1000*60*60));  // modulo pour calculer le nombre d'heures restant
     const minutes = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000*60));  // modulo pour calculer le nombre de minutes restantes
     const secondes = Math.floor((distanceBase % (1000 * 60)) / (1000));  // modulo pour calculer le nombre de secondes restantes
-
-    text.innerText = `${days}j ${hours}h ${minutes}m ${secondes}s`;
+    
+    text.innerText = `Dans ${days}J  ${hours}h ${minutes}m ${secondes}s`;
 }
 
 getChrono();
